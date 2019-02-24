2af2c0b1bd354d88f74299b42a1cd570417a1ddb
Steve Hook <steve.hook@gmail.com>
2019-02-24 23:21:17 +0000

    Refactor params method

--- app/controllers/polynomials_controller.rb
+++ app/controllers/polynomials_controller.rb
@@ -4,6 +4,12 @@ require_relative '../../lib/polynomial'
 
 class PolynomialsController < ApplicationController
   def differentiate
-    render plain: Polynomial.new(params[:polynomials].split('/').map(&:to_i)).differentiate.to_s
+    render plain: Polynomial.new(coefficient_params).differentiate.to_s
+  end
+
+  private
+
+  def coefficient_params
+    params[:polynomials].split('/').map(&:to_i)
   end
 end
--- spec/requests/polynomials_controller_spec.rb
+++ spec/requests/polynomials_controller_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe PolynomialsController, type: :request do
       expect(response).to be_successful
     end
 
-    it 'returns the correct result for the constant polynomial `f(x) = 1`' do
-      get '/differentiate/1'
-      expect(response.body).to eql '0'
+    it 'returns the correct result for the constant polynomial `f(x) = x^2+2x+3`' do
+      get '/differentiate/1/2/3'
+      expect(response.body).to eql '2x+2'
     end
   end
 end
